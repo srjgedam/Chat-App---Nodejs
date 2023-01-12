90da66fea7987e4668500a886b03d7788648da09
Suraj Gedam <srjgedam@gmai.com>
2023-01-13 00:12:52 +0530

    Innit comment

---
+++ src/app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
+const { ifError } = require("assert");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+console.log(publicDirectoryPath);
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+    socket.emit("message", generateMessage("Admin", "Welcome"));
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generateMessage("Admin", `${user.username} has joined`));
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+  });
+
+  socket.on("sendMessage", (msg, callback) => {
+    const user = getUser(socket.id);
+
+    const filter = new Filter();
+
+    if (filter.isProfane(msg)) {
+      return callback("Profanity is not allowed");
+    }
+
+    io.to(user.room).emit("message", generateMessage(user.username, msg));
+    callback();
+  });
+
+  socket.on("sendLocation", (coords, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
+    );
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("Admin", `${user.username} has left the chat`)
+      );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+
+  console.log("Socket IO new connection");
+});
+
+server.listen(port, () => {
+  console.log("server is up and running on port 3000");
+});
